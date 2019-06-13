7a1ab596780f85efeb04b6edb224f21854e67640
refdinal22 <refdinal22@gmail.com>
2019-06-13 21:23:00 +0700

    Adding Maps Intent

--- app/src/main/java/com/example/gita/eatreminder/MainActivity.java
+++ app/src/main/java/com/example/gita/eatreminder/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.gita.eatreminder;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String googleMap = "com.google.android.apps.maps"; // identitas package aplikasi google masps android
+    Uri gmmIntentUri;
+    Intent mapIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +31,18 @@ public class MainActivity extends AppCompatActivity {
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                gmmIntentUri = Uri.parse("geo:0,0?q=restaurants");
+
+                mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+
+                mapIntent.setPackage(googleMap);
+
+                if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(mapIntent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Google Maps Belum Terinstal. Install Terlebih dahulu.",
+                            Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
